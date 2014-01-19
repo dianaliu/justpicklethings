5d0387412e457180d35196dda3cea796060dc7f7
dianaliu <diana.g.liu@gmail.com>
2014-01-18 22:43:07 -0500

    MVP, yo

--- main.py
+++ main.py
@@ -11,11 +11,30 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__.split('.')[0])
 
+from instagram.client import InstagramAPI
+from random import choice
 
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
   """ Return hello template at application root URL."""
-  return render_template('hello.html', name=name)
+  # TODO: Set up separate account for #justpicklethings
+  access_token = "46268358.26174be.936f60ef309d4414bc6e9e9fb60a92eb"
+  api = InstagramAPI(access_token=access_token)
+  pickles, next = api.tag_recent_media(10, 0, "pickle")
+
+  choice_pickle = choice(pickles)
+  # Truncate long captions
+  pickle_caption = choice_pickle.caption.text.lower()
+  pickle_caption = (pickle_caption[:110] + "...") if len(pickle_caption) > 110 else pickle_caption
+
+  pickle_link = choice_pickle.link
+  pickle_user = choice_pickle.user.username
+  pickle_image_link = choice_pickle.images["standard_resolution"].url
+
+  # TODO: Download generated image with canvas
+  # TODO: Post the best ones to a an instagram account or tweet them.
+
+  return render_template('hello.html', caption=pickle_caption, username=pickle_user, link=pickle_link, image=pickle_image_link)
 
 
